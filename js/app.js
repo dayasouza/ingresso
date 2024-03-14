278b08c9ba86b482dafbd13e71324fc4335defff
Daya Souza <dayanalvesouza@gmail.com>
2024-03-13 21:25:47 -0300

    alterado de var para let

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 // Função para comprar ingressos
 function comprar() {
     // Obtém o tipo de ingresso selecionado pelo usuário
-    var tipoIngresso = document.getElementById("tipo-ingresso").value;   
+    let tipoIngresso = document.getElementById("tipo-ingresso").value;   
 
     // Obtém a quantidade digitada pelo usuário
-    var quantidade = parseInt(document.getElementById("qtd").value);
+    let quantidade = parseInt(document.getElementById("qtd").value);
 
     // Obtém a quantidade disponível atual do tipo de ingresso selecionado
-    var quantidadeDisponivel = parseInt(document.getElementById("qtd-" + tipoIngresso).textContent);
+    let quantidadeDisponivel = parseInt(document.getElementById("qtd-" + tipoIngresso).textContent);
 
     // Verifica se a quantidade desejada é maior que a quantidade disponível
     if (quantidade > quantidadeDisponivel) {
@@ -16,6 +16,6 @@ function comprar() {
     }
 
     // Atualiza a quantidade disponível subtraindo a quantidade comprada
-    var novaQuantidade = quantidadeDisponivel - quantidade;
+    let novaQuantidade = quantidadeDisponivel - quantidade;
     document.getElementById("qtd-" + tipoIngresso).textContent = novaQuantidade;
 }
