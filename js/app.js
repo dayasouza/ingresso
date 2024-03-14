ecf60f6d947ae6056bedcced6fbe05e574ed93da
Daya Souza <dayanalvesouza@gmail.com>
2024-03-13 21:34:35 -0300

    adicionada função de validação

--- js/app.js
+++ js/app.js
@@ -1,13 +1,20 @@
 // Função para comprar ingressos
 function comprar() {
     // Obtém o tipo de ingresso selecionado pelo usuário
-    let tipoIngresso = document.getElementById("tipo-ingresso").value;   
+    var tipoIngresso = document.getElementById("tipo-ingresso").value;
 
     // Obtém a quantidade digitada pelo usuário
-    let quantidade = parseInt(document.getElementById("qtd").value);
+    var quantidadeInput = document.getElementById("qtd").value;
+    var quantidade = parseInt(quantidadeInput);
+
+    // Verifica se a quantidade é um número positivo
+    if (isNaN(quantidade) || quantidade <= 0) {
+        alert("Por favor, insira uma quantidade válida.");
+        return; // Sai da função se a quantidade não for válida
+    }
 
     // Obtém a quantidade disponível atual do tipo de ingresso selecionado
-    let quantidadeDisponivel = parseInt(document.getElementById("qtd-" + tipoIngresso).textContent);
+    var quantidadeDisponivel = parseInt(document.getElementById("qtd-" + tipoIngresso).textContent);
 
     // Verifica se a quantidade desejada é maior que a quantidade disponível
     if (quantidade > quantidadeDisponivel) {
@@ -16,6 +23,6 @@ function comprar() {
     }
 
     // Atualiza a quantidade disponível subtraindo a quantidade comprada
-    let novaQuantidade = quantidadeDisponivel - quantidade;
+    var novaQuantidade = quantidadeDisponivel - quantidade;
     document.getElementById("qtd-" + tipoIngresso).textContent = novaQuantidade;
 }
